8ab15208f42c7d79ed08be25b63eed87ae7a66dc
Everton Rocha <evertonrps@outlook.com>
2019-10-17 10:05:59 -0300

    Criação de novo projeto

---
+++ tests/GameLibrary.BDD.Tests/Developers/CreateDeveloperSteps.cs
@@ -0,0 +1,69 @@
+﻿using GameLibrary.BDD.Tests.Utils;
+using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace GameLibrary.BDD.Tests.Developers
+{
+    [Binding]
+    public class CreateDeveloperSteps
+    {
+        private SeleniumHelper Browser = new SeleniumHelper();
+
+        [Given(@"clica no link da area de developer")]
+        public void DadoClicaNoLinkDaAreaDeDeveloper()
+        {           
+            Browser.ClicarBotaoPorId("button-basic-desenvolvedoras");
+            Browser.ClicarLinkPorTexto("Cadastrar");
+            Browser.EsperaTelaCarregar(5000);
+        }
+        
+        [Given(@"Acessa a tela de cadastro")]
+        public void DadoAcessaATelaDeCadastro()
+        {
+           var result = Browser.ValidarConteudoUrl("/developers/new");
+            Assert.True(result);
+        }
+        
+        [Given(@"Preenche os dados do formulario")]
+        public void DadoPreencheOsDadosDoFormulario(Table table)
+        {
+            Browser.PreencherTextBoxPorId(table.Rows[0][0], table.Rows[0][1]);//Name
+            Browser.PreencherTextBoxPorId(table.Rows[1][0], table.Rows[1][1]);//Founded
+            Browser.PreencherTextBoxPorId(table.Rows[2][0], table.Rows[2][1]);//Website    
+        }
+        
+        [When(@"Clica no botao Cadastrar")]
+        public void QuandoClicaNoBotaoCadastrar()
+        {
+            Browser.ClicarBotaoPorId("btnSalvar");
+        }
+        
+        [Then(@"Recebe uma mensagem de developer cadastrado  com sucesso")]
+        public void EntaoRecebeUmaMensagemDeDeveloperCadastradoComSucesso()
+        {
+            string text = Browser.ObterTextoElementoPorClasse("toast-message");
+            var ret = Browser.ObterListaPorPath("//*[@id='divErro']/ul/li");
+
+            Assert.Equal("Developer Registrado com Sucesso!", text);
+        }
+        
+        [Then(@"exclui o developer cadastrado")]
+        public void EntaoExcluiODeveloperCadastrado()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Recebe uma mensagem de nome obrigatorio")]
+        public void EntaoRecebeUmaMensagemDeNomeObrigatorio()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Recebe uma mensagem de data de fundacao invalida")]
+        public void EntaoRecebeUmaMensagemDeDataDeFundacaoInvalida()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
---
+++ tests/GameLibrary.BDD.Tests/Utils/ConfigurationHelper.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace GameLibrary.BDD.Tests.Utils
+{
+    public class ConfigurationHelper
+    {
+        /*
+         using Microsoft.Extensions.Configuration;
+         using Microsoft.Extensions.Configuration.FileExtensions;
+         using Microsoft.Extensions.Configuration.Json;
+             */
+
+        private readonly IConfiguration config;
+        public ConfigurationHelper()
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+
+            config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+
+        }
+
+        public static ConfigurationHelper Build()
+        {
+            return new ConfigurationHelper();
+        }
+        public  string BaseUrl => config["BaseUrl"];
+
+        public  string HomeUrl => config["HomeUrl"];
+
+        public  string RegisterUrl => string.Format("{0}{1}", BaseUrl, config["RegisterUrl"]);
+
+        public  string LoginUrl => string.Format("{0}{1}", BaseUrl, config["LoginUrl"]);
+
+        public string ChromeDriver => string.Format("{0}", config["ChromeDriver"]);
+
+        public  string FolderPath => Path.GetDirectoryName(Path.GetDirectoryName(Directory.GetCurrentDirectory()));
+
+        public  string FolderPicture => string.Format("{0}{1}", FolderPath, config["FolderPicture"]);
+    }
+}
